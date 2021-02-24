dcfc5d4b5d8c785dde8d4d6cf6ea45a5a1eb791f
Sean Moriarity <smoriarity.5@gmail.com>
2021-02-24 11:00:40 -0500

    All initializers as defn

--- lib/axon/initializers.ex
+++ lib/axon/initializers.ex
@@ -3,7 +3,6 @@ defmodule Axon.Initializers do
   Common parameter initializers.
   """
 
-  # TODO: These should all be defn
   # TODO: Add random keys
 
   import Nx.Defn
@@ -62,12 +61,7 @@ defmodule Axon.Initializers do
   """
   defn identity(opts \\ []) do
     opts = keyword!(opts, [:shape, type: {:f, 32}])
-    transform(opts[:shape], &assert_rank(&1, 2))
-
-    Nx.as_type(
-      Nx.equal(Nx.iota(opts[:shape], axis: 0), Nx.iota(opts[:shape], axis: 1)),
-      opts[:type]
-    )
+    Nx.eye(opts[:shape], type: opts[:type])
   end
 
   @doc """
@@ -105,22 +99,26 @@ defmodule Axon.Initializers do
   @doc """
   Initializes parameters with a [Lecun uniform](http://yann.lecun.com/exdb/publis/pdf/lecun-98b.pdf) initializer.
   """
-  def lecun_uniform(shape, opts \\ []) do
-    type = opts[:type] || {:f, 32}
-    scale = opts[:scale] || 1.0
-    variance_scaling(shape, type: type, scale: scale, mode: :fan_in, distribution: :uniform)
+  defn lecun_uniform(opts \\ []) do
+    opts = keyword!(opts, [:shape, type: {:f, 32}, scale: 1.0])
+    variance_scaling(
+      shape: opts[:shape],
+      type: opts[:type],
+      scale: opts[:scale],
+      mode: :fan_in,
+      distribution: :uniform
+    )
   end
 
   @doc """
   Initializers parameters with a [Lecun normal](http://yann.lecun.com/exdb/publis/pdf/lecun-98b.pdf) initializer.
   """
-  def lecun_normal(shape, opts \\ []) do
-    type = opts[:type] || {:f, 32}
-    scale = opts[:scale] || 1.0
-
-    variance_scaling(shape,
-      type: type,
-      scale: scale,
+  defn lecun_normal(opts \\ []) do
+    opts = keyword!(opts, [:shape, type: {:f, 32}, scale: 1.0])
+    variance_scaling(
+      shape: opts[:shape],
+      type: opts[:type],
+      scale: opts[:scale],
       mode: :fan_in,
       distribution: :truncated_normal
     )
@@ -129,22 +127,26 @@ defmodule Axon.Initializers do
   @doc """
   Initializes parameters with a [Glorot uniform](http://proceedings.mlr.press/v9/glorot10a.html) initializer.
   """
-  def glorot_uniform(shape, opts \\ []) do
-    type = opts[:type] || {:f, 32}
-    scale = opts[:scale] || 1.0
-    variance_scaling(shape, type: type, scale: scale, mode: :fan_avg, distribution: :uniform)
+  defn glorot_uniform(opts \\ []) do
+    opts = keyword!(opts, [:shape, type: {:f, 32}, scale: 1.0])
+    variance_scaling(
+      shape: opts[:shape],
+      type: opts[:type],
+      scale: opts[:scale],
+      mode: :fan_avg,
+      distribution: :uniform
+    )
   end
 
   @doc """
   Initializes parameters with a [Glorot normal](http://proceedings.mlr.press/v9/glorot10a.html) initializer.
   """
-  def glorot_normal(shape, opts \\ []) do
-    type = opts[:type] || {:f, 32}
-    scale = opts[:scale] || 1.0
-
-    variance_scaling(shape,
-      type: type,
-      scale: scale,
+  defn glorot_normal(opts \\ []) do
+    opts = keyword!(opts, [:shape, type: {:f, 32}, scale: 1.0])
+    variance_scaling(
+      shape: opts[:shape],
+      type: opts[:type],
+      scale: opts[:scale],
       mode: :fan_avg,
       distribution: :truncated_normal
     )
@@ -153,22 +155,26 @@ defmodule Axon.Initializers do
   @doc """
   Initializes parameters with a [He uniform](https://www.cv-foundation.org/openaccess/content_iccv_2015/html/He_Delving_Deep_into_ICCV_2015_paper.html) initializer.
   """
-  def he_uniform(shape, opts \\ []) do
-    type = opts[:type] || {:f, 32}
-    scale = opts[:scale] || 2.0
-    variance_scaling(shape, type: type, scale: scale, mode: :fan_in, distribution: :uniform)
+  defn he_uniform(opts \\ []) do
+    opts = keyword!(opts, [:shape, type: {:f, 32}, scale: 2.0])
+    variance_scaling(
+      shape: opts[:shape],
+      type: opts[:type],
+      scale: opts[:scale],
+      mode: :fan_in,
+      distribution: :uniform
+    )
   end
 
   @doc """
   Initializes parameters with a [He normal](https://www.cv-foundation.org/openaccess/content_iccv_2015/html/He_Delving_Deep_into_ICCV_2015_paper.html) initializer.
   """
-  def he_normal(shape, opts \\ []) do
-    type = opts[:type] || {:f, 32}
-    scale = opts[:scale] || 2.0
-
-    variance_scaling(shape,
-      type: type,
-      scale: scale,
+  defn he_normal(opts \\ []) do
+    opts = keyword!(opts, [:shape, type: {:f, 32}, scale: 2.0])
+    variance_scaling(
+      shape: opts[:shape],
+      type: opts[:type],
+      scale: opts[:scale],
       mode: :fan_in,
       distribution: :truncated_normal
     )
@@ -178,61 +184,59 @@ defmodule Axon.Initializers do
   Initializes parameters with variance scaling according to
   the given distribution and mode.
   """
-  def variance_scaling(shape, opts \\ []) do
-    type = opts[:type] || {:f, 32}
-    scale = opts[:scale] || 1.0e-2
-    mode = opts[:mode] || :fan_in
-    distribution = opts[:distribution] || :normal
+  defn variance_scaling(opts \\ []) do
+    opts = keyword!(opts, [:shape, type: {:f, 32}, scale: 1.0e-2, mode: :fan_in, distribution: :normal])
 
-    {fan_in, fan_out} = compute_fans(shape)
+    fans = transform(opts[:shape], &compute_fans/1)
+    denominator = transform({fans, opts[:mode]},
+      fn
+        {{fan_in, _}, :fan_in} -> fan_in
+        {{_, fan_out}, :fan_out} -> fan_out
+        {{fan_in, fan_out}, :fan_avg} -> (fan_in + fan_out) / 2.0
+      end)
 
-    denominator =
-      case mode do
-        :fan_in -> fan_in
-        :fan_out -> fan_out
-        :fan_avg -> (fan_in + fan_out) / 2.0
+    variance = Nx.divide(Nx.tensor(opts[:scale], type: opts[:type]), denominator)
+
+    var_opts = transform(opts, &Keyword.take(&1, [:shape, :type]))
+
+    transform({opts[:distribution], variance, var_opts},
+      fn
+        {:normal, variance, opts} ->
+          var_normal(variance, opts)
+        {:uniform, variance, opts} ->
+          var_uniform(variance, opts)
+        {:truncated_normal, variance, opts} ->
+          var_uniform(variance, opts)
+        {dist, _, _} ->
+          raise ArgumentError, "invalid distribution #{inspect(dist)} passed to variance_scaling/1"
       end
+    )
+  end
 
-    variance = Nx.divide(Nx.tensor(scale, type: type), Nx.tensor(denominator, type: type))
+  # Variance scaling branches
+  defnp var_normal(variance, opts \\ []) do
+    opts = keyword!(opts, [:shape, type: {:f, 32}])
+    Nx.random_normal(opts[:shape], type: opts[:type]) * Nx.sqrt(variance)
+  end
 
-    case distribution do
-      :normal ->
-        Nx.random_normal(shape, type: type) * Nx.sqrt(variance)
+  defnp var_uniform(variance, opts \\ []) do
+    opts = keyword!(opts, [:shape, type: {:f, 32}])
+    Nx.random_uniform(opts[:shape], type: opts[:type]) * Nx.sqrt(Nx.multiply(3.0, variance))
+  end
 
-      :uniform ->
-        Nx.random_uniform(shape, type: type) * Nx.sqrt(Nx.multiply(3.0, variance))
-
-      :truncated_normal ->
-        stddev = Nx.divide(Nx.sqrt(variance), Nx.tensor(0.87962566103423978, type: type))
-        Nx.multiply(Nx.clip(Nx.random_normal(shape, type: type), -2.0, 2.0), stddev)
-    end
+  defnp var_truncated(variance, opts \\ []) do
+    stddev = Nx.divide(Nx.sqrt(variance), 0.87962566103423978)
+    Nx.multiply(Nx.clip(Nx.random_normal(opts[:shape], type: opts[:type]), -2.0, 2.0), stddev)
   end
 
   # Helpers
 
-  defp assert_rank(shape, rank) do
-    n = tuple_size(shape)
-
-    unless n == rank,
-      do:
-        raise(
-          ArgumentError,
-          "invalid rank for shape #{inspect(shape)}" <>
-            " expected shape to have rank #{rank}, got" <>
-            " #{n}"
-        )
-  end
-
   defp compute_fans(shape) do
     receptive_field_size =
-      tuple_product(shape, tuple_size(shape)) / elem(shape, 0) / elem(shape, 1)
+      Nx.size(shape) / elem(shape, 0) / elem(shape, 1)
 
     fan_in = elem(shape, 0) * receptive_field_size
     fan_out = elem(shape, 1) * receptive_field_size
     {fan_in, fan_out}
   end
-
-  # TODO: Replace with Tuple.product on Elixir v1.12
-  defp tuple_product(_tuple, 0), do: 1
-  defp tuple_product(tuple, i), do: :erlang.element(i, tuple) * tuple_product(tuple, i - 1)
 end
