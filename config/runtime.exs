11067b7cedb69212c28339a8765dacbabea07817
Sean Moriarity <smoriarity.5@gmail.com>
2021-03-21 15:52:56 -0400

    Remove config

--- config/runtime.exs
+++
@@ -1,9 +0,0 @@
-import Config
-
-target = System.get_env("EXLA_TARGET", "host")
-
-config :exla, :clients, default: [platform: :cuda]
-
-config :logger, :console,
-  format: "\n$time [$level] $metadata $levelpad$message\n",
-  metadata: [:domain, :file, :line]
