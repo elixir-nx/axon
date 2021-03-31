217c3eb2bd95a00b766f5f1cd396021e691ebe4e
Sean Moriarity <smoriarity.5@gmail.com>
2021-03-31 13:17:39 -0400

    Initial training documentation

--- lib/axon/training.ex
+++ lib/axon/training.ex
@@ -1,10 +1,33 @@
 defmodule Axon.Training do
   @moduledoc """
-  Training Loop.
+  Abstractions for training and validating machine learning models.
   """
   require Axon
 
-  def step(objective_fn, update_fn) do
+  @doc """
+  Represents a single training step using an objective
+  function and an update function.
+
+  The objective function is used to obtain loss and gradients with respect
+  to model parameters, inputs, and targets. Update function is used to scale
+  gradients and apply updates in the style of gradient-based optimization
+  methods.
+
+  Returns an arity-4 step function of the form:
+
+      step(parameters, optimizer_state, inputs, targets) :: {new_parameters, new_optimizer_state, loss}
+
+  `objective_fn` is of the form:
+
+      objective(parameters, inputs, targets) :: loss
+
+  Objective function must be scalar-valued and differentiable.
+
+  `update_fn` is of the form:
+
+      update(gradients, state) :: {updates, new_state}
+  """
+  def step(objective_fn, update_fn) when is_function(objective_fn) and is_function(update_fn) do
     fn params, update_state, input, target ->
       {batch_loss, gradients} = Nx.Defn.Kernel.value_and_grad(params, &objective_fn.(&1, input, target))
       {updates, new_update_state} = update_fn.(gradients, update_state)
@@ -12,6 +35,28 @@ defmodule Axon.Training do
     end
   end
 
+  @doc """
+  Represents a single training step using an Axon model,
+  loss function, and update function.
+
+  `model` and `loss` are combined into an objective function:
+
+      objective_fn =
+        fn params, input, target ->
+          preds = Axon.predict(model, params, input)
+          loss.(target, preds)
+        end
+
+  Returns an arity-4 step function of the form:
+
+      step(parameters, optimizer_state, inputs, targets) :: {new_parameters, new_optimizer_state, loss}
+
+  `loss` must be an arity-2, scalar-valued, differentiable function.
+
+  `update_fn` is of the form:
+
+      update(gradients, state) :: {updates, new_state}
+  """
   def step(%Axon{} = model, loss, update_fn) when is_function(loss, 2) do
     objective_fn =
       fn params, input, target ->
@@ -21,11 +66,38 @@ defmodule Axon.Training do
     step(objective_fn, update_fn)
   end
 
+  @doc """
+  Represents a single training step using an Axon model,
+  loss atom, and update function.
+
+  `model` and `loss` are combined into an objective function:
+
+      objective_fn =
+        fn params, input, target ->
+          preds = Axon.predict(model, params, input)
+          loss.(target, preds)
+        end
+
+  where `loss` is taken from one of the functions in `Axon.Losses`.
+
+  `update_fn` is of the form:
+
+      update(gradients, state) :: {updates, new_state}
+  """
   def step(%Axon{} = model, loss, update_fn) when is_atom(loss) do
     loss_fn = &apply(Axon.Losses, loss, [&1, &2, [reduction: :mean]])
     step(model, loss_fn, update_fn)
   end
 
+  @doc """
+  Implements a common training loop.
+
+  ## Options
+
+    * `epochs` - number of epochs to train for. Defaults to `5`.
+    * `compiler` - `defn` compiler to use to run training loop.
+      Defaults to `Nx.Defn.Evaluator`.
+  """
   def train(step_fn, model, optim_init_fn, inputs, targets, opts \\ []) do
     epochs = opts[:epochs] || 5
     compiler = opts[:compiler] || Nx.Defn.Evaluator
