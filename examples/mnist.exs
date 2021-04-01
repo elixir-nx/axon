43ca25e1602f0db29371b7e4538e9cabefeab1cc
Sean Moriarity <smoriarity.5@gmail.com>
2021-04-01 13:27:53 -0400

    Rollback MNIST

--- examples/mnist.exs
+++ examples/mnist.exs
@@ -50,18 +50,10 @@ defmodule MNIST do
   end
 end
 
-residual =
-  fn x ->
-    x
-    |> Axon.dense(128)
-    |> Axon.add(x, name: "add")
-  end
-
 model =
   Axon.input({nil, 784})
   |> Axon.dense(128, activation: :relu)
   |> Axon.layer_norm()
-  |> residual.()
   |> Axon.dropout()
   |> Axon.dense(10, activation: :softmax)
 
