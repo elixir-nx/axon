bcfba964e33c9dfda93987a317a6edac3996a574
float2 <nicholas.cheredov@gmail.com>
2021-09-23 19:32:14 +0300

    Add: Mnist example Mix.install update

--- examples/fashionmnist_autoencoder.exs
+++ examples/fashionmnist_autoencoder.exs
@@ -28,37 +28,42 @@ defmodule Fashionmist do
     end
   end
 
-  transform_images =
-    fn {bin, type, shape} ->
-      bin
-      |> Nx.from_binary(type)
-      |> Nx.reshape({elem(shape, 0), 1, 28, 28})
-      |> Nx.divide(255.0)
-      |> Nx.to_batched_list(32)
-    end
+  def run do
+    transform_images =
+      fn {bin, type, shape} ->
+        bin
+        |> Nx.from_binary(type)
+        |> Nx.reshape({elem(shape, 0), 1, 28, 28})
+        |> Nx.divide(255.0)
+        |> Nx.to_batched_list(32)
+      end
 
-  {train_images, _} = Scidata.FashionMNIST.download(transform_images: transform_images)
+    {train_images, _} = Scidata.FashionMNIST.download(transform_images: transform_images)
 
-  sample_image =
-    train_images
-    |> hd()
-    |> Nx.slice_axis(0, 1, 0)
-    |> Nx.reshape({1, 1, 28, 28})
+    sample_image =
+      train_images
+      |> hd()
+      |> Nx.slice_axis(0, 1, 0)
+      |> Nx.reshape({1, 1, 28, 28})
 
-  sample_image |> Nx.to_heatmap() |> IO.inspect
+    sample_image |> Nx.to_heatmap() |> IO.inspect
 
-  model = Autoencoder.model(64)
+    model = Autoencoder.model(64)
 
-  IO.inspect model
+    IO.inspect model
 
-  final_training_state =
+    final_training_state =
+      model
+      |> Axon.Training.step(:mean_squared_error, Axon.Optimizers.adam(0.01), metrics: [:mean_absolute_error])
+      |> Axon.Training.train(train_images, train_images, epochs: 5, compiler: EXLA)
+
     model
-    |> Axon.Training.step(:mean_squared_error, Axon.Optimizers.adam(0.01), metrics: [:mean_absolute_error])
-    |> Axon.Training.train(train_images, train_images, epochs: 5, compiler: EXLA)
+    |> Axon.predict(final_training_state[:params], sample_image, compiler: EXLA)
+    |> Nx.reshape({1, 28, 28})
+    |> Nx.to_heatmap()
+    |> IO.inspect()
+  end
 
-  model
-  |> Axon.predict(final_training_state[:params], sample_image, compiler: EXLA)
-  |> Nx.reshape({1, 28, 28})
-  |> Nx.to_heatmap()
-  |> IO.inspect()
 end
+
+Fashionmist.run()
--- examples/mnist.exs
+++ examples/mnist.exs
@@ -1,47 +1,61 @@
-require Axon
+Mix.install([
+  {:axon, "~> 0.1.0-dev", github: "elixir-nx/axon", branch: "main"},
+  {:exla, github: "elixir-nx/exla", sparse: "exla"},
+  {:nx, "~> 0.1.0-dev", github: "elixir-nx/nx", sparse: "nx", override: true},
+  {:scidata, "~> 0.1.1"},
+])
 
-transform_images =
-  fn {bin, type, shape} ->
-    bin
-    |> Nx.from_binary(type)
-    |> Nx.reshape({elem(shape, 0), 784})
-    |> Nx.divide(255.0)
-    |> Nx.to_batched_list(32)
+defmodule Mnist do
+  require Axon
+
+  def run do
+    transform_images =
+      fn {bin, type, shape} ->
+        bin
+        |> Nx.from_binary(type)
+        |> Nx.reshape({elem(shape, 0), 784})
+        |> Nx.divide(255.0)
+        |> Nx.to_batched_list(32)
+      end
+
+    transform_labels =
+      fn {bin, type, _} ->
+        bin
+        |> Nx.from_binary(type)
+        |> Nx.new_axis(-1)
+        |> Nx.equal(Nx.tensor(Enum.to_list(0..9)))
+        |> Nx.to_batched_list(32)
+      end
+
+    {train_images, train_labels} = Scidata.MNIST.download(transform_images: transform_images, transform_labels: transform_labels)
+
+    IO.inspect train_images |> hd() |> Nx.slice_axis(0, 1, 0) |> Nx.reshape({1, 28, 28}) |> Nx.to_heatmap()
+
+    model =
+      Axon.input({nil, 784})
+      |> Axon.dense(128, activation: :relu)
+      |> Axon.dropout()
+      |> Axon.dense(10, activation: :softmax)
+
+    IO.inspect model
+
+    final_training_state =
+      model
+      |> Axon.Training.step(:categorical_cross_entropy, Axon.Optimizers.adamw(0.005), metrics: [:accuracy])
+      |> Axon.Training.train(train_images, train_labels, epochs: 10, compiler: EXLA, log_every: 100)
+
+    test_images = train_images |> hd() |> Nx.slice_axis(10, 3, 0)
+
+    IO.inspect test_images |> Nx.reshape({3, 28, 28}) |> Nx.to_heatmap()
+
+    prediction =
+      model
+      |> Axon.predict(final_training_state[:params], test_images)
+      |> Nx.argmax(axis: -1)
+
+    prediction
   end
 
-transform_labels =
-  fn {bin, type, _} ->
-    bin
-    |> Nx.from_binary(type)
-    |> Nx.new_axis(-1)
-    |> Nx.equal(Nx.tensor(Enum.to_list(0..9)))
-    |> Nx.to_batched_list(32)
-  end
+end
 
-{train_images, train_labels} = Scidata.MNIST.download(transform_images: transform_images, transform_labels: transform_labels)
-
-IO.inspect train_images |> hd() |> Nx.slice_axis(0, 1, 0) |> Nx.reshape({1, 28, 28}) |> Nx.to_heatmap()
-
-model =
-  Axon.input({nil, 784})
-  |> Axon.dense(128, activation: :relu)
-  |> Axon.dropout()
-  |> Axon.dense(10, activation: :softmax)
-
-IO.inspect model
-
-final_training_state =
-  model
-  |> Axon.Training.step(:categorical_cross_entropy, Axon.Optimizers.adamw(0.005), metrics: [:accuracy])
-  |> Axon.Training.train(train_images, train_labels, epochs: 10, compiler: EXLA, log_every: 100)
-
-test_images = train_images |> hd() |> Nx.slice_axis(10, 3, 0)
-
-IO.inspect test_images |> Nx.reshape({3, 28, 28}) |> Nx.to_heatmap()
-
-prediction =
-  model
-  |> Axon.predict(final_training_state[:params], test_images)
-  |> Nx.argmax(axis: -1)
-
-prediction
+Mnist.run()
