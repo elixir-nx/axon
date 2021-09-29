b8cdee732d3603a055c7f498bf4b1f8614b7531e
float2 <nicholas.cheredov@gmail.com>
2021-09-29 21:26:47 +0300

    Ref: Cifar example Mix.install update

--- examples/cifar10.exs
+++ examples/cifar10.exs
@@ -8,69 +8,76 @@ Mix.install([
 defmodule Cifar do
   require Axon
 
-  def run do
-    transform_images =
-      fn {bin, type, shape} ->
-        bin
-        |> Nx.from_binary(type)
-        |> Nx.reshape({elem(shape, 0), 3, 32, 32})
-        |> Nx.divide(255.0)
-        |> Nx.to_batched_list(32)
-      end
+  defp transform_images({bin, type, shape}) do
+    bin
+    |> Nx.from_binary(type)
+    |> Nx.reshape({elem(shape, 0), 3, 32, 32})
+    |> Nx.divide(255.0)
+    |> Nx.to_batched_list(32)
+  end
 
-    transform_labels =
-      fn {bin, type, _} ->
-        bin
-        |> Nx.from_binary(type)
-        |> Nx.new_axis(-1)
-        |> Nx.equal(Nx.tensor(Enum.to_list(0..9)))
-        |> Nx.to_batched_list(32)
-      end
+  defp transform_labels({bin, type, _}) do
+    bin
+    |> Nx.from_binary(type)
+    |> Nx.new_axis(-1)
+    |> Nx.equal(Nx.tensor(Enum.to_list(0..9)))
+    |> Nx.to_batched_list(32)
+  end
 
-    {train_images, train_labels} = Scidata.CIFAR10.download(transform_images: transform_images, transform_labels: transform_labels)
-
-    train_images
+  defp view_images(images, {start_index, len}) do
+    images
     |> hd()
-    |> Nx.slice_axis(0, 1, 0)
-    |> Nx.reshape({3, 32, 32})
-    |> Nx.mean(axes: [0], keep_axes: true)
+    |> Nx.slice_axis(start_index, len, 0)
+    |> Nx.mean(axes: [1], keep_axes: true)
     |> Nx.to_heatmap()
     |> IO.inspect
+  end
 
-    model =
-      Axon.input({nil, 3, 32, 32})
-      |> Axon.conv(32, kernel_size: {3, 3}, activation: :relu)
-      |> Axon.batch_norm()
-      |> Axon.max_pool(kernel_size: {2, 2})
-      |> Axon.conv(64, kernel_size: {3, 3}, activation: :relu)
-      |> Axon.spatial_dropout()
-      |> Axon.batch_norm()
-      |> Axon.max_pool(kernel_size: {2, 2})
-      |> Axon.conv(32, kernel_size: {3, 3}, activation: :relu)
-      |> Axon.batch_norm()
-      |> Axon.flatten()
-      |> Axon.dense(64, activation: :relu)
-      |> Axon.dropout()
-      |> Axon.dense(10, activation: :softmax)
+  defp build_model(input_shape) do
+    Axon.input(input_shape)
+    |> Axon.conv(32, kernel_size: {3, 3}, activation: :relu)
+    |> Axon.batch_norm()
+    |> Axon.max_pool(kernel_size: {2, 2})
+    |> Axon.conv(64, kernel_size: {3, 3}, activation: :relu)
+    |> Axon.spatial_dropout()
+    |> Axon.batch_norm()
+    |> Axon.max_pool(kernel_size: {2, 2})
+    |> Axon.conv(32, kernel_size: {3, 3}, activation: :relu)
+    |> Axon.batch_norm()
+    |> Axon.flatten()
+    |> Axon.dense(64, activation: :relu)
+    |> Axon.dropout()
+    |> Axon.dense(10, activation: :softmax)
+  end
 
+  defp perform_training(model, {train_images, train_labels}, epochs) do
+    model
+    |> Axon.Training.step(:categorical_cross_entropy, Axon.Optimizers.sgd(0.01), metrics: [:accuracy])
+    |> Axon.Training.train(train_images, train_labels, epochs: epochs, compiler: EXLA)
+    |> Nx.backend_transfer()
+  end
+
+  def run do
+
+    {train_images, train_labels} = Scidata.CIFAR10.download(transform_images: &transform_images/1, transform_labels: &transform_labels/1)
+
+    view_images(train_images, {0, 1})
+
+    model = build_model({nil, 3, 32, 32})
     IO.inspect model
 
-    final_training_state =
+    final_training_state = 
       model
-      |> Axon.Training.step(:categorical_cross_entropy, Axon.Optimizers.sgd(0.01), metrics: [:accuracy])
-      |> Axon.Training.train(train_images, train_labels, epochs: 20, compiler: EXLA)
-      |> Nx.backend_transfer()
+      |> perform_training({train_images, train_labels}, 20)
       |> IO.inspect()
 
     test_images = train_images |> hd() |> Nx.slice_axis(10, 3, 0)
+    view_images(train_images, {10, 3})
 
-    IO.inspect test_images |> Nx.mean(axes: [1], keep_axes: true) |> Nx.to_heatmap()
-
-    prediction =
-      model
-      |> Axon.predict(final_training_state[:params], test_images)
-      |> Nx.argmax(axis: -1)
-      |> IO.inspect
+    model
+    |> Axon.predict(final_training_state[:params], test_images)
+    |> Nx.argmax(axis: -1)
+    |> IO.inspect
   end
 end
 
