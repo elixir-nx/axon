c7159abc139dd5615e9a2e2d993796f2f570571a
float2 <nicholas.cheredov@gmail.com>
2021-09-30 19:41:36 +0300

    Ref: XOR example Mix.install update

--- examples/xor.exs
+++ examples/xor.exs
@@ -8,32 +8,40 @@ Mix.install([
 defmodule XOR do
   require Axon
 
+  defp build_model(input_shape1, input_shape2) do
+    inp1 = Axon.input(input_shape1)
+    inp2 = Axon.input(input_shape2)
+    inp1
+    |> Axon.concatenate(inp2)
+    |> Axon.dense(8, activation: :tanh)
+    |> Axon.dense(1, activation: :sigmoid)
+  end
+
+  defp build_data do
+    for _ <- 1..1000 do
+      x1 = for _ <- 1..32, do: [Enum.random(0..1)]
+      x2 = for _ <- 1..32, do: [Enum.random(0..1)]
+      {Nx.tensor(x1), Nx.tensor(x2)}
+    end
+  end
+
+  defp train_model(model, {data, targets}, epochs) do
+    model
+    |> Axon.Training.step(:binary_cross_entropy, Axon.Optimizers.sgd(0.01))
+    |> Axon.Training.train(data, targets, epochs: epochs)
+  end
+
   def run do
-    inp1 = Axon.input({:nil, 1})
-    inp2 = Axon.input({:nil, 1})
+    model = build_model({:nil, 1}, {:nil, 1})
 
-    model =
-      inp1
-      |> Axon.concatenate(inp2)
-      |> Axon.dense(8, activation: :tanh)
-      |> Axon.dense(1, activation: :sigmoid)
-
-    data =
-      for _ <- 1..1000 do
-        x1 = for _ <- 1..32, do: [Enum.random(0..1)]
-        x2 = for _ <- 1..32, do: [Enum.random(0..1)]
-        {Nx.tensor(x1), Nx.tensor(x2)}
-      end
+    data = build_data()
 
     targets =
       for {x1, x2} <- data do
         Nx.logical_xor(x1, x2)
       end
 
-    final_training_state =
-      model
-      |> Axon.Training.step(:binary_cross_entropy, Axon.Optimizers.sgd(0.01))
-      |> Axon.Training.train(data, targets, epochs: 10)
+    final_training_state = train_model(model, {data, targets}, 10)
 
     IO.inspect Axon.predict(model, final_training_state[:params], {Nx.tensor([[0]]), Nx.tensor([[1]])})
   end
