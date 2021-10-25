bb61027ca20962d7b906fe3aa191061b1a2fb375
Sean Moriarity <smoriarity.5@gmail.com>
2021-10-25 12:15:23 -0400

    Update shape tests and error message

--- lib/axon/shape.ex
+++ lib/axon/shape.ex
@@ -1322,7 +1322,23 @@ defmodule Axon.Shape do
   end
 
   @doc """
-  Calculates the base shape of a split operation.
+  Calculates the base shape and slice size of a split operation.
+
+  ## Examples
+
+      iex> Axon.Shape.split({nil, 1, 10}, 2, -1)
+      {5, {nil, 1, 5}}
+
+      iex> Axon.Shape.split({32, 1, 10}, 16, 0)
+      {2, {2, 1, 10}}
+
+  ### Error cases
+
+      iex> Axon.Shape.split({nil, 1, 10}, 2, 0)
+      ** (ArgumentError) cannot split along batch dimension with dynamic batch size, please provide a static (non-nil) batch size
+
+      iex> Axon.Shape.split({nil, 5, 10}, 2, 1)
+      ** (ArgumentError) unable to create 2 even splits along axis 1 of size 5
   """
   def split(shape, n, axis) do
     nil_names = List.duplicate(nil, Nx.rank(shape))
@@ -1339,7 +1355,7 @@ defmodule Axon.Shape do
 
     unless rem(elem(shape, axis), n) == 0 do
       raise ArgumentError,
-            "unable to create even split of size #{n} along axis #{axis}" <>
+            "unable to create #{n} even splits along axis #{axis}" <>
               " of size #{elem(shape, axis)}"
     end
 
