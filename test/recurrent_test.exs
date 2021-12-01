00f487c3ad9767dc638fcf64497645787176dad1
Sean Moriarity <smoriarity.5@gmail.com>
2021-12-01 06:12:23 -0500

    fix recurrent tests

--- test/recurrent_test.exs
+++ test/recurrent_test.exs
@@ -5,24 +5,28 @@ defmodule Axon.RecurrentTest do
 
   describe "dynamic_unroll" do
     test "computes carry and output identical to static_unroll" do
-      input = Nx.random_uniform({1, 4, 2})
-      carry = {Nx.random_uniform({1, 1, 8})}
+      input = Nx.iota({1, 4, 2}, type: {:f, 32})
+      carry = {Nx.iota({1, 1, 8}, type: {:f, 32})}
 
       input_kernel =
-        {Nx.random_uniform({2, 8}), Nx.random_uniform({2, 8}), Nx.random_uniform({2, 8})}
+        {Nx.iota({2, 8}, type: {:f, 32}), Nx.iota({2, 8}, type: {:f, 32}),
+         Nx.iota({2, 8}, type: {:f, 32})}
 
       hidden_kernel =
-        {Nx.random_uniform({8, 8}), Nx.random_uniform({8, 8}), Nx.random_uniform({8, 8})}
+        {Nx.iota({8, 8}, type: {:f, 32}), Nx.iota({8, 8}, type: {:f, 32}),
+         Nx.iota({8, 8}, type: {:f, 32})}
 
-      bias = {Nx.random_uniform({}), Nx.random_uniform({}), Nx.random_uniform({}), Nx.random_uniform({})}
+      bias =
+        {Nx.iota({}, type: {:f, 32}), Nx.iota({}, type: {:f, 32}), Nx.iota({}, type: {:f, 32}),
+         Nx.iota({}, type: {:f, 32})}
 
       cell_fn = &Axon.Recurrent.gru_cell/5
 
       {{s_carry}, s_output} =
-          Axon.Recurrent.static_unroll(cell_fn, input, carry, input_kernel, hidden_kernel, bias)
+        Axon.Recurrent.static_unroll(cell_fn, input, carry, input_kernel, hidden_kernel, bias)
 
       {{d_carry}, d_output} =
-          Axon.Recurrent.dynamic_unroll(cell_fn, input, carry, input_kernel, hidden_kernel, bias)
+        Axon.Recurrent.dynamic_unroll(cell_fn, input, carry, input_kernel, hidden_kernel, bias)
 
       assert s_carry == d_carry
       assert s_output == d_output
@@ -30,8 +34,7 @@ defmodule Axon.RecurrentTest do
 
     defn grad_static_hidden_output(input, carry, input_kernel, hidden_kernel, bias, cell_fn) do
       grad(hidden_kernel, fn x ->
-        {_, output} =
-          Axon.Recurrent.static_unroll(cell_fn, input, carry, input_kernel, x, bias)
+        {_, output} = Axon.Recurrent.static_unroll(cell_fn, input, carry, input_kernel, x, bias)
 
         Nx.mean(output)
       end)
@@ -39,35 +42,44 @@ defmodule Axon.RecurrentTest do
 
     defn grad_dynamic_hidden_output(input, carry, input_kernel, hidden_kernel, bias, cell_fn) do
       grad(hidden_kernel, fn x ->
-        {_, output} =
-          Axon.Recurrent.dynamic_unroll(cell_fn, input, carry, input_kernel, x, bias)
+        {_, output} = Axon.Recurrent.dynamic_unroll(cell_fn, input, carry, input_kernel, x, bias)
 
         Nx.mean(output)
       end)
     end
 
     test "computes gradient identical to static unroll for hidden kernel w.r.t. output" do
-      input = Nx.random_uniform({1, 4, 2})
-      carry = {Nx.random_uniform({1, 1, 8})}
+      input = Nx.iota({1, 4, 2}, type: {:f, 32})
+      carry = {Nx.iota({1, 1, 8}, type: {:f, 32})}
 
       input_kernel =
-        {Nx.random_uniform({2, 8}), Nx.random_uniform({2, 8}), Nx.random_uniform({2, 8})}
+        {Nx.iota({2, 8}, type: {:f, 32}), Nx.iota({2, 8}, type: {:f, 32}),
+         Nx.iota({2, 8}, type: {:f, 32})}
 
       hidden_kernel =
-        {Nx.random_uniform({8, 8}), Nx.random_uniform({8, 8}), Nx.random_uniform({8, 8})}
+        {Nx.iota({8, 8}, type: {:f, 32}), Nx.iota({8, 8}, type: {:f, 32}),
+         Nx.iota({8, 8}, type: {:f, 32})}
 
-      bias = {Nx.random_uniform({}), Nx.random_uniform({}), Nx.random_uniform({}), Nx.random_uniform({})}
+      bias =
+        {Nx.iota({}, type: {:f, 32}), Nx.iota({}, type: {:f, 32}), Nx.iota({}, type: {:f, 32}),
+         Nx.iota({}, type: {:f, 32})}
 
       cell_fn = &Axon.Recurrent.gru_cell/5
 
       assert grad_static_hidden_output(input, carry, input_kernel, hidden_kernel, bias, cell_fn) ==
-          grad_dynamic_hidden_output(input, carry, input_kernel, hidden_kernel, bias, cell_fn)
+               grad_dynamic_hidden_output(
+                 input,
+                 carry,
+                 input_kernel,
+                 hidden_kernel,
+                 bias,
+                 cell_fn
+               )
     end
 
     defn grad_static_hidden_carry(input, carry, input_kernel, hidden_kernel, bias, cell_fn) do
       grad(hidden_kernel, fn x ->
-        {{carry}, _} =
-          Axon.Recurrent.static_unroll(cell_fn, input, carry, input_kernel, x, bias)
+        {{carry}, _} = Axon.Recurrent.static_unroll(cell_fn, input, carry, input_kernel, x, bias)
 
         Nx.mean(carry)
       end)
@@ -75,35 +87,37 @@ defmodule Axon.RecurrentTest do
 
     defn grad_dynamic_hidden_carry(input, carry, input_kernel, hidden_kernel, bias, cell_fn) do
       grad(hidden_kernel, fn x ->
-        {{carry}, _} =
-          Axon.Recurrent.dynamic_unroll(cell_fn, input, carry, input_kernel, x, bias)
+        {{carry}, _} = Axon.Recurrent.dynamic_unroll(cell_fn, input, carry, input_kernel, x, bias)
 
         Nx.mean(carry)
       end)
     end
 
     test "computes gradient identical to static_unroll for hidden kernel w.r.t carry" do
-      input = Nx.random_uniform({1, 4, 2})
-      carry = {Nx.random_uniform({1, 1, 8})}
+      input = Nx.iota({1, 4, 2}, type: {:f, 32})
+      carry = {Nx.iota({1, 1, 8}, type: {:f, 32})}
 
       input_kernel =
-        {Nx.random_uniform({2, 8}), Nx.random_uniform({2, 8}), Nx.random_uniform({2, 8})}
+        {Nx.iota({2, 8}, type: {:f, 32}), Nx.iota({2, 8}, type: {:f, 32}),
+         Nx.iota({2, 8}, type: {:f, 32})}
 
       hidden_kernel =
-        {Nx.random_uniform({8, 8}), Nx.random_uniform({8, 8}), Nx.random_uniform({8, 8})}
+        {Nx.iota({8, 8}, type: {:f, 32}), Nx.iota({8, 8}, type: {:f, 32}),
+         Nx.iota({8, 8}, type: {:f, 32})}
 
-      bias = {Nx.random_uniform({}), Nx.random_uniform({}), Nx.random_uniform({}), Nx.random_uniform({})}
+      bias =
+        {Nx.iota({}, type: {:f, 32}), Nx.iota({}, type: {:f, 32}), Nx.iota({}, type: {:f, 32}),
+         Nx.iota({}, type: {:f, 32})}
 
       cell_fn = &Axon.Recurrent.gru_cell/5
 
       assert grad_static_hidden_carry(input, carry, input_kernel, hidden_kernel, bias, cell_fn) ==
-        grad_dynamic_hidden_carry(input, carry, input_kernel, hidden_kernel, bias, cell_fn)
+               grad_dynamic_hidden_carry(input, carry, input_kernel, hidden_kernel, bias, cell_fn)
     end
 
     defn grad_static_input_output(input, carry, input_kernel, hidden_kernel, bias, cell_fn) do
       grad(input_kernel, fn x ->
-        {_, output} =
-          Axon.Recurrent.static_unroll(cell_fn, input, carry, x, hidden_kernel, bias)
+        {_, output} = Axon.Recurrent.static_unroll(cell_fn, input, carry, x, hidden_kernel, bias)
 
         Nx.mean(output)
       end)
@@ -111,35 +125,37 @@ defmodule Axon.RecurrentTest do
 
     defn grad_dynamic_input_output(input, carry, input_kernel, hidden_kernel, bias, cell_fn) do
       grad(input_kernel, fn x ->
-        {_, output} =
-          Axon.Recurrent.dynamic_unroll(cell_fn, input, carry, x, hidden_kernel, bias)
+        {_, output} = Axon.Recurrent.dynamic_unroll(cell_fn, input, carry, x, hidden_kernel, bias)
 
         Nx.mean(output)
       end)
     end
 
     test "computes gradient identical to static unroll for input kernel w.r.t. output" do
-      input = Nx.random_uniform({1, 4, 2})
-      carry = {Nx.random_uniform({1, 1, 8})}
+      input = Nx.iota({1, 4, 2}, type: {:f, 32})
+      carry = {Nx.iota({1, 1, 8}, type: {:f, 32})}
 
       input_kernel =
-        {Nx.random_uniform({2, 8}), Nx.random_uniform({2, 8}), Nx.random_uniform({2, 8})}
+        {Nx.iota({2, 8}, type: {:f, 32}), Nx.iota({2, 8}, type: {:f, 32}),
+         Nx.iota({2, 8}, type: {:f, 32})}
 
       hidden_kernel =
-        {Nx.random_uniform({8, 8}), Nx.random_uniform({8, 8}), Nx.random_uniform({8, 8})}
+        {Nx.iota({8, 8}, type: {:f, 32}), Nx.iota({8, 8}, type: {:f, 32}),
+         Nx.iota({8, 8}, type: {:f, 32})}
 
-      bias = {Nx.random_uniform({}), Nx.random_uniform({}), Nx.random_uniform({}), Nx.random_uniform({})}
+      bias =
+        {Nx.iota({}, type: {:f, 32}), Nx.iota({}, type: {:f, 32}), Nx.iota({}, type: {:f, 32}),
+         Nx.iota({}, type: {:f, 32})}
 
       cell_fn = &Axon.Recurrent.gru_cell/5
 
       assert grad_static_input_output(input, carry, input_kernel, hidden_kernel, bias, cell_fn) ==
-          grad_dynamic_input_output(input, carry, input_kernel, hidden_kernel, bias, cell_fn)
+               grad_dynamic_input_output(input, carry, input_kernel, hidden_kernel, bias, cell_fn)
     end
 
     defn grad_static_input_carry(input, carry, input_kernel, hidden_kernel, bias, cell_fn) do
       grad(input_kernel, fn x ->
-        {{carry}, _} =
-          Axon.Recurrent.static_unroll(cell_fn, input, carry, x, hidden_kernel, bias)
+        {{carry}, _} = Axon.Recurrent.static_unroll(cell_fn, input, carry, x, hidden_kernel, bias)
 
         Nx.mean(carry)
       end)
@@ -155,21 +171,25 @@ defmodule Axon.RecurrentTest do
     end
 
     test "computes gradient identical to static unroll for input kernel w.r.t. carry" do
-      input = Nx.random_uniform({1, 4, 2})
-      carry = {Nx.random_uniform({1, 1, 8})}
+      input = Nx.iota({1, 4, 2}, type: {:f, 32})
+      carry = {Nx.iota({1, 1, 8}, type: {:f, 32})}
 
       input_kernel =
-        {Nx.random_uniform({2, 8}), Nx.random_uniform({2, 8}), Nx.random_uniform({2, 8})}
+        {Nx.iota({2, 8}, type: {:f, 32}), Nx.iota({2, 8}, type: {:f, 32}),
+         Nx.iota({2, 8}, type: {:f, 32})}
 
       hidden_kernel =
-        {Nx.random_uniform({8, 8}), Nx.random_uniform({8, 8}), Nx.random_uniform({8, 8})}
+        {Nx.iota({8, 8}, type: {:f, 32}), Nx.iota({8, 8}, type: {:f, 32}),
+         Nx.iota({8, 8}, type: {:f, 32})}
 
-      bias = {Nx.random_uniform({}), Nx.random_uniform({}), Nx.random_uniform({}), Nx.random_uniform({})}
+      bias =
+        {Nx.iota({}, type: {:f, 32}), Nx.iota({}, type: {:f, 32}), Nx.iota({}, type: {:f, 32}),
+         Nx.iota({}, type: {:f, 32})}
 
       cell_fn = &Axon.Recurrent.gru_cell/5
 
       assert grad_static_input_carry(input, carry, input_kernel, hidden_kernel, bias, cell_fn) ==
-          grad_dynamic_input_carry(input, carry, input_kernel, hidden_kernel, bias, cell_fn)
+               grad_dynamic_input_carry(input, carry, input_kernel, hidden_kernel, bias, cell_fn)
     end
 
     defn grad_static_bias_output(input, carry, input_kernel, hidden_kernel, bias, cell_fn) do
@@ -191,21 +211,25 @@ defmodule Axon.RecurrentTest do
     end
 
     test "computes gradient identical to static unroll for bias w.r.t. output" do
-      input = Nx.random_uniform({1, 4, 2})
-      carry = {Nx.random_uniform({1, 1, 8})}
+      input = Nx.iota({1, 4, 2}, type: {:f, 32})
+      carry = {Nx.iota({1, 1, 8}, type: {:f, 32})}
 
       input_kernel =
-        {Nx.random_uniform({2, 8}), Nx.random_uniform({2, 8}), Nx.random_uniform({2, 8})}
+        {Nx.iota({2, 8}, type: {:f, 32}), Nx.iota({2, 8}, type: {:f, 32}),
+         Nx.iota({2, 8}, type: {:f, 32})}
 
       hidden_kernel =
-        {Nx.random_uniform({8, 8}), Nx.random_uniform({8, 8}), Nx.random_uniform({8, 8})}
+        {Nx.iota({8, 8}, type: {:f, 32}), Nx.iota({8, 8}, type: {:f, 32}),
+         Nx.iota({8, 8}, type: {:f, 32})}
 
-      bias = {Nx.random_uniform({}), Nx.random_uniform({}), Nx.random_uniform({}), Nx.random_uniform({})}
+      bias =
+        {Nx.iota({}, type: {:f, 32}), Nx.iota({}, type: {:f, 32}), Nx.iota({}, type: {:f, 32}),
+         Nx.iota({}, type: {:f, 32})}
 
       cell_fn = &Axon.Recurrent.gru_cell/5
 
       assert grad_static_bias_output(input, carry, input_kernel, hidden_kernel, bias, cell_fn) ==
-          grad_dynamic_bias_output(input, carry, input_kernel, hidden_kernel, bias, cell_fn)
+               grad_dynamic_bias_output(input, carry, input_kernel, hidden_kernel, bias, cell_fn)
     end
 
     defn grad_static_bias_carry(input, carry, input_kernel, hidden_kernel, bias, cell_fn) do
@@ -227,21 +251,25 @@ defmodule Axon.RecurrentTest do
     end
 
     test "computes gradient identical to static unroll for bias w.r.t. carry" do
-      input = Nx.random_uniform({1, 4, 2})
-      carry = {Nx.random_uniform({1, 1, 8})}
+      input = Nx.iota({1, 4, 2}, type: {:f, 32})
+      carry = {Nx.iota({1, 1, 8}, type: {:f, 32})}
 
       input_kernel =
-        {Nx.random_uniform({2, 8}), Nx.random_uniform({2, 8}), Nx.random_uniform({2, 8})}
+        {Nx.iota({2, 8}, type: {:f, 32}), Nx.iota({2, 8}, type: {:f, 32}),
+         Nx.iota({2, 8}, type: {:f, 32})}
 
       hidden_kernel =
-        {Nx.random_uniform({8, 8}), Nx.random_uniform({8, 8}), Nx.random_uniform({8, 8})}
+        {Nx.iota({8, 8}, type: {:f, 32}), Nx.iota({8, 8}, type: {:f, 32}),
+         Nx.iota({8, 8}, type: {:f, 32})}
 
-      bias = {Nx.random_uniform({}), Nx.random_uniform({}), Nx.random_uniform({}), Nx.random_uniform({})}
+      bias =
+        {Nx.iota({}, type: {:f, 32}), Nx.iota({}, type: {:f, 32}), Nx.iota({}, type: {:f, 32}),
+         Nx.iota({}, type: {:f, 32})}
 
       cell_fn = &Axon.Recurrent.gru_cell/5
 
       assert grad_static_bias_carry(input, carry, input_kernel, hidden_kernel, bias, cell_fn) ==
-          grad_dynamic_bias_carry(input, carry, input_kernel, hidden_kernel, bias, cell_fn)
+               grad_dynamic_bias_carry(input, carry, input_kernel, hidden_kernel, bias, cell_fn)
     end
   end
 end
