098fab8bcf1b511d21a23a1a38bc3a061cfbfb06
floatn <nicholas.cheredov@gmail.com>
2022-01-21 20:53:12 +0300

    Fix: test

--- test/losses_test.exs
+++ test/losses_test.exs
@@ -214,7 +214,7 @@ defmodule Axon.LossesTest do
           ]
         ])
 
-      assert Axon.Losses.connectionist_temporal_classification({l_true, y_true}, y_pred) ==
+      assert Axon.Losses.connectionist_temporal_classification({l_true, y_true}, y_pred, reduction: :mean) ==
                Nx.tensor([2.472621440887451])
     end
   end
