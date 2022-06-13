6c8a4f40097d7475afd2f6fe1c167e7f844c4c7f
Sean Moriarity <smoriarity.5@gmail.com>
2022-06-13 18:39:23 -0400

    Use git dep for now

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Axon.MixProject do
     if path = System.get_env("AXON_NX_PATH") do
       [path: path, override: true]
     else
-      []
+      [github: "elixir-nx/nx", sparse: "nx", override: true]
     end
   end
 
@@ -47,7 +47,7 @@ defmodule Axon.MixProject do
     if path = System.get_env("AXON_EXLA_PATH") do
       [path: path, only: :test]
     else
-      [only: :test]
+      [github: "elixir-nx/nx", sparse: "exla", only: :test]
     end
   end
 
