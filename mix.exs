d616ba4f8ffc495facafcbfb797371074d98e997
Sean Moriarity <smoriarity.5@gmail.com>
2022-07-16 20:19:51 -0400

    Update deps

--- mix.exs
+++ mix.exs
@@ -56,7 +56,7 @@ defmodule Axon.MixProject do
     if path = System.get_env("AXON_NX_PATH") do
       [path: path, override: true]
     else
-      []
+      [github: "elixir-nx/nx", sparse: "nx", override: true]
     end
   end
 
@@ -64,7 +64,7 @@ defmodule Axon.MixProject do
     if path = System.get_env("AXON_EXLA_PATH") do
       [path: path]
     else
-      []
+      [github: "elixir-nx/nx", sparse: "exla"]
     end
   end
 
