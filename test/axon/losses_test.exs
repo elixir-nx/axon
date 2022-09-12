e9b795d132978e3923edcb46ecff8aa77611c86b
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2022-09-12 18:22:08 -0300

    test: losses

--- test/axon/losses_test.exs
+++ test/axon/losses_test.exs
@@ -1,8 +1,6 @@
 defmodule Axon.LossesTest do
-  use ExUnit.Case, async: true
+  use Axon.Case, async: true
   doctest Axon.Losses
-
-  import AxonTestUtil
 
   describe "binary_cross_entropy" do
     test "supports class weights" do
@@ -11,20 +9,24 @@ defmodule Axon.LossesTest do
       pos = 0.5
       neg = 0.8
 
-      assert Axon.Losses.binary_cross_entropy(y_true, y_pred,
-               positive_weight: pos,
-               negative_weight: neg,
-               reduction: :mean
-             ) ==
-               Nx.tensor(0.546828031539917)
+      assert_equal(
+        Axon.Losses.binary_cross_entropy(y_true, y_pred,
+          positive_weight: pos,
+          negative_weight: neg,
+          reduction: :mean
+        ),
+        Nx.tensor(0.546828031539917)
+      )
     end
 
     test "supports from_logits" do
       y_true = Nx.tensor([0, 1, 0, 1, 0])
       y_pred = Nx.tensor([15.0, -10.0, 6.0, 2.0, -1.0])
 
-      assert Axon.Losses.binary_cross_entropy(y_true, y_pred, from_logits: true, reduction: :mean) ==
-               Nx.tensor(6.2885422706604)
+      assert_all_close(
+        Axon.Losses.binary_cross_entropy(y_true, y_pred, from_logits: true, reduction: :mean),
+        Nx.tensor(6.2885422706604)
+      )
     end
 
     test "raises on y_true shape not equal to y_pred" do
@@ -45,55 +47,70 @@ defmodule Axon.LossesTest do
       y_pred = Nx.tensor([[0.1, 0.8, 0.1], [0.4, 0.2, 0.4], [0.15, 0.25, 0.6]])
       weights = [0.3, 0.2, 0.2]
 
-      assert Axon.Losses.categorical_cross_entropy(y_true, y_pred,
-               class_weights: weights,
-               reduction: :none
-             ) == Nx.tensor([0.04462870582938194, 0.27488723397254944, 0.1021651178598404])
+      assert_equal(
+        Axon.Losses.categorical_cross_entropy(y_true, y_pred,
+          class_weights: weights,
+          reduction: :none
+        ),
+        Nx.tensor([0.04462870582938194, 0.27488723397254944, 0.1021651178598404])
+      )
 
-      assert Axon.Losses.categorical_cross_entropy(y_true, y_pred,
-               class_weights: weights,
-               reduction: :mean
-             ) == Nx.tensor(0.6024014353752136)
+      assert_all_close(
+        Axon.Losses.categorical_cross_entropy(y_true, y_pred,
+          class_weights: weights,
+          reduction: :mean
+        ),
+        Nx.tensor(0.6024014353752136)
+      )
 
-      assert Axon.Losses.categorical_cross_entropy(y_true, y_pred,
-               class_weights: weights,
-               reduction: :sum
-             ) == Nx.tensor(0.4216810464859009)
+      assert_equal(
+        Axon.Losses.categorical_cross_entropy(y_true, y_pred,
+          class_weights: weights,
+          reduction: :sum
+        ),
+        Nx.tensor(0.4216810464859009)
+      )
     end
 
     test "supports from_logits" do
       y_true = Nx.tensor([[0, 1, 0], [1, 0, 0], [0, 0, 1]])
       y_pred = Nx.tensor([[15.0, 2.0, -22.0], [-5.0, -2.0, 3.0], [2.0, 1.96, 1.20]])
 
-      assert Axon.Losses.categorical_cross_entropy(y_true, y_pred,
-               from_logits: true,
-               reduction: :mean
-             ) ==
-               Nx.tensor(7.562242031097412)
+      assert_equal(
+        Axon.Losses.categorical_cross_entropy(y_true, y_pred,
+          from_logits: true,
+          reduction: :mean
+        ),
+        Nx.tensor(7.562242031097412)
+      )
     end
 
     test "supports sparse 1-d targets, from logits" do
       y_true = Nx.tensor([1, 0, 2])
       y_pred = Nx.tensor([[15.0, 2.0, -22.0], [-5.0, -2.0, 3.0], [2.0, 1.96, 1.20]])
 
-      assert Axon.Losses.categorical_cross_entropy(y_true, y_pred,
-               from_logits: true,
-               sparse: true,
-               reduction: :mean
-             ) ==
-               Nx.tensor(7.562242031097412)
+      assert_equal(
+        Axon.Losses.categorical_cross_entropy(y_true, y_pred,
+          from_logits: true,
+          sparse: true,
+          reduction: :mean
+        ),
+        Nx.tensor(7.562242031097412)
+      )
     end
 
     test "supports sparse 2-d targets, from logits" do
       y_true = Nx.tensor([[1], [0], [2]])
       y_pred = Nx.tensor([[15.0, 2.0, -22.0], [-5.0, -2.0, 3.0], [2.0, 1.96, 1.20]])
 
-      assert Axon.Losses.categorical_cross_entropy(y_true, y_pred,
-               from_logits: true,
-               sparse: true,
-               reduction: :mean
-             ) ==
-               Nx.tensor(7.562242031097412)
+      assert_equal(
+        Axon.Losses.categorical_cross_entropy(y_true, y_pred,
+          from_logits: true,
+          sparse: true,
+          reduction: :mean
+        ),
+        Nx.tensor(7.562242031097412)
+      )
     end
 
     test "supports sparse 1-d targets, from softmax" do
@@ -104,12 +121,14 @@ defmodule Axon.LossesTest do
           Nx.tensor([[15.0, 2.0, -22.0], [-5.0, -2.0, 3.0], [2.0, 1.96, 1.20]])
         )
 
-      assert Axon.Losses.categorical_cross_entropy(y_true, y_pred,
-               from_logits: false,
-               sparse: true,
-               reduction: :mean
-             ) ==
-               Nx.tensor(7.562242031097412)
+      assert_equal(
+        Axon.Losses.categorical_cross_entropy(y_true, y_pred,
+          from_logits: false,
+          sparse: true,
+          reduction: :mean
+        ),
+        Nx.tensor(7.562242031097412)
+      )
     end
 
     test "supports sparse 2-d targets, from softmax" do
@@ -120,16 +139,20 @@ defmodule Axon.LossesTest do
           Nx.tensor([[15.0, 2.0, -22.0], [-5.0, -2.0, 3.0], [2.0, 1.96, 1.20]])
         )
 
-      assert Axon.Losses.categorical_cross_entropy(y_true, y_pred,
-               from_logits: false,
-               sparse: true,
-               reduction: :mean
-             ) ==
-               Nx.tensor(7.562242031097412)
+      assert_equal(
+        Axon.Losses.categorical_cross_entropy(y_true, y_pred,
+          from_logits: false,
+          sparse: true,
+          reduction: :mean
+        ),
+        Nx.tensor(7.562242031097412)
+      )
     end
   end
 
   describe "ctcloss" do
+    @tag :skip_exla
+    @tag :skip_torchx
     test "value for basic case" do
       y_true =
         Nx.tensor([
@@ -144,6 +167,8 @@ defmodule Axon.LossesTest do
                Nx.tensor([8.08642292022705, 8.933040618896484])
     end
 
+    @tag :skip_exla
+    @tag :skip_torchx
     test "oversize don't contribute" do
       y_true1 =
         Nx.tensor([
@@ -163,9 +188,11 @@ defmodule Axon.LossesTest do
       loss1 = Axon.Losses.connectionist_temporal_classification({l_true, y_true1}, y_pred)
       loss2 = Axon.Losses.connectionist_temporal_classification({l_true, y_true2}, y_pred)
 
-      assert loss1 == loss2
+      assert_equal(loss1, loss2)
     end
 
+    @tag :skip_exla
+    @tag :skip_torchx
     test "value for complex case" do
       y_true = Nx.tensor([[2, 3, 4, 1]])
       l_true = Nx.tensor([4])
@@ -190,6 +217,8 @@ defmodule Axon.LossesTest do
                Nx.tensor([10.772387504577637])
     end
 
+    @tag :skip_exla
+    @tag :skip_torchx
     test ":mean reduction" do
       y_true =
         Nx.tensor([
@@ -227,10 +256,12 @@ defmodule Axon.LossesTest do
           ]
         ])
 
-      assert Axon.Losses.connectionist_temporal_classification({l_true, y_true}, y_pred,
-               reduction: :mean
-             ) ==
-               Nx.tensor(2.472621440887451)
+      assert_equal(
+        Axon.Losses.connectionist_temporal_classification({l_true, y_true}, y_pred,
+          reduction: :mean
+        ),
+        Nx.tensor(2.472621440887451)
+      )
     end
   end
 
